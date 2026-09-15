26167b1db727c679626b955d4b13e0b5ef735b28
Nick <humarnick@gmail.com>
2026-09-15 20:31:59 +0100

    Added Open CV and MediaPipe trackers

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
 from drone import Drone
+
 import settings, sys
-
-
-
-def create_drone(connection, baud = None):
-    return Drone(connection, baud)
-
-
-# run waypoint mission
-def drone_arm_mission(drone, path):
-    drone.guided_arm_takeoff(10)
-    drone.upload_mission(drone.load_waypoint(path))
-    drone.change_flight_mode("auto")
-    drone.waypoint_tracker()
-    drone.monitor_until_disarmed()
-
-
 
 # This function checks if Pi or desktop is running the code
 # Don't need to manually change everytime!!!
@@ -23,11 +8,11 @@ def pi_or_sim():
     if len(sys.argv) > 1 and sys.argv[1] == "pi":
         connection = settings.connection_string
         baud = settings.baud_rate
-        drone = create_drone(connection, baud)
+        drone = Drone(connection, baud)
 
     else:
         connection = "tcp:127.0.0.1:5763"
-        drone = create_drone(connection)
+        drone = Drone(connection)
 
     return drone
 
@@ -35,5 +20,5 @@ def pi_or_sim():
 
 if __name__ == "__main__":
     drone_1 = pi_or_sim()
-    drone_arm_mission(drone_1, settings.path)
-    drone_1.clear_mission()
+    drone_1.drone_takeoff(2)
+
---
+++ vision/full_vision.py
@@ -0,0 +1,114 @@
+import cv2 as cv
+import mediapipe as mp
+import time
+
+
+mp_hands = mp.solutions.hands # this is the whole Hands module, think of a toolbox, from that I use the tool .Hands
+mp_draw = mp.solutions.drawing_utils # for drawing the connections
+
+
+cap = cv.VideoCapture(0) # Camera index
+
+
+# Resolution
+cap.set(3, 1200) # 3 = width
+cap.set(4, 720)  # 4 = height
+
+
+def main():
+    # loading Hands detection module into memory, then passing each frame into it
+    with mp_hands.Hands(
+    max_num_hands = 2,
+    min_detection_confidence = 0.7, # higher confidence is more accurate
+    min_tracking_confidence = 0.7
+    ) as hands:
+        while True:
+            attempt = 0
+            success, frame = cap.read() # returns bool and frame from camera
+
+            # Loop to try again if first read fails, breaks when success = True
+            while not success and attempt < 5:
+                time.sleep(0.1)
+                success, frame = cap.read()
+                attempt += 1
+
+            # 5 fails triggers this
+            if not success:
+                print("Failed to read frame")
+                break
+
+            # Flipping image so more intuitive
+            frame = cv.flip(frame, 1)
+            h, w, _ = frame.shape 
+
+            # OCV gives BGR, convert to RGB here for MediaPipe
+            rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+
+            # process() runs the hand detection model on the RGB converted frame
+            # main thing it returns is multi_hand_landmarks
+            # 21 landmark points on each hand
+            results = hands.process(rgb)
+
+            fingers_up = 0
+            # if its found hands, draw landmarks
+            if results.multi_hand_landmarks:
+                for hand, hand_landmarks in enumerate(results.multi_hand_landmarks):
+                    mp_draw.draw_landmarks(
+                        frame,
+                        hand_landmarks,
+                        mp_hands.HAND_CONNECTIONS
+                    )
+
+                    # Draw labels
+                    finger_tips = {
+                        "Thumb": hand_landmarks.landmark[4],
+                        "Index": hand_landmarks.landmark[8],
+                        "Middle": hand_landmarks.landmark[12],
+                        "Ring": hand_landmarks.landmark[16],
+                        "Pinky": hand_landmarks.landmark[20]
+                    }
+
+                    for name, landmark in finger_tips.items():
+                        x, y = int(landmark.x * w), int(landmark.y * h)
+                        cv.putText(
+                            frame,
+                            name,
+                            (x, y - 20),
+                            cv.FONT_HERSHEY_SIMPLEX,
+                            0.5,
+                            (255, 255, 255),
+                            1
+                        )
+
+                    which_hand = results.multi_handedness[hand].classification[0].label
+
+                    for tip in range(4, 21, 4): # Just hitting tips (4, 8, 12, 16, 20)
+                        if tip == 4:
+                            # Trying to catch thumbs here, tricky!
+                            if which_hand == "Left":
+                                if hand_landmarks.landmark[tip].x > hand_landmarks.landmark[tip - 1].x:
+                                    fingers_up += 1
+                            else:
+                                if hand_landmarks.landmark[tip].x < hand_landmarks.landmark[tip - 1].x:
+                                    fingers_up += 1
+                        else:            
+                            if hand_landmarks.landmark[tip].y < hand_landmarks.landmark[tip - 2].y:
+                                fingers_up += 1
+
+                print(f"Fingers up: {fingers_up}")
+
+            # Opens windows
+            cv.imshow("Frame", frame)
+
+            # Closes on Q press
+            if cv.waitKey(1) == ord("q"):
+                break
+
+    cap.release()
+    cv.destroyAllWindows()
+
+
+
+
+if __name__ == "__main__":
+    main()
