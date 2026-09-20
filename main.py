cf98054014816738332dbbd6fc407c66857fa895
Nick Desktop <humarnick@gmail.com>
2026-09-20 17:27:56 +0100

    TRY/EXCEPT blocks on main.py

--- main.py
+++ main.py
@@ -34,16 +34,25 @@ if __name__ == "__main__":
         9: drone.land_disarm,
         }
 
-    while True:
-        gesture = video.video_functions.finger_counter()
+    try: # Due to running on the Pi, I'll be cancelling the execution with CTRL C
+        while True:
+            gesture = video.video_functions.finger_counter()
 
-        if gesture in missions:
-            missions[gesture]()
-        elif gesture == 10:
-            video.video_functions.release_camera()
-            drone.drone_disarm()
-            drone.close()
-            print("Drone disconnecting...")
-            break
-        else:
-            print("No mission mapped to that gesture.")
+            if gesture in missions:
+                try: # Try/Except block here catches bad mission calls, prints an error and keeps looking for gestures
+                    missions[gesture]()
+                except Exception as e:
+                    print(f"Missiong for gesture {gesture} failed: {e}")
+            elif gesture == 10:
+                break
+            else:
+                print("No mission mapped to that gesture")
+
+    except KeyboardInterrupt: # Keyboard trigger
+        print("Interrupted by user")
+        
+    finally: # This always runs before the program closes
+        video.video_functions.release_camera()
+        drone.drone_disarm()
+        drone.close()
+        print("Drone disarming and disconnecting...")
