b458ba9cec766d1d0ed1bf9680e795a07a602c08
Nick Desktop <humarnick@gmail.com>
2026-09-26 15:02:19 +0100

    typo

--- scripts/video_capture.py
+++ scripts/video_capture.py
@@ -2,7 +2,7 @@
 MOVE THE DRONE IN A CIRCLE PATTERN
 '''
 
-import sys, os, time, cv2 as cv
+import sys, os, time
 
 # This is to help import from working directory
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -11,31 +11,27 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # will stop the program crashing when run on desktop with webcam
 try:
     from picamera2 import Picamera2
-    from picamera2.encoders import H264encoder
+    from picamera2.encoders import H264Encoder
     from picamera2.outputs import FfmpegOutput
     has_pi_camera = True
+except ImportError as e:
+    has_pi_camera = False
+    print(f"No PiCamera libraries found: {e}")
+    
 
+    cap = Picamera2()
     encoder = H264Encoder(bitrate = 10000000)
     output = FfmpegOutput("/home/pummet/my-drone-project/video_recordings/video.mp4")
 
     camera_size = (640, 480)
 
-    if has_pi_camera:
-        cap = Picamera2()
-    else:
-        # DirectShow opens way faster than the default MSMF backend on Windows
-        cap = cv.VideoCapture(0, cv.CAP_DSHOW if sys.platform == "win32" else cv.CAP_ANY) # Camera index
+    video_config = cap.create_video_configuration()
+    cap.configure(video_config)
+    print("start record")
+    cap.start()
+    cap.start_recording(encoder, output)
 
-    if has_pi_camera:
-        video_config = cap.create_video_configuration()
-        cap.configure(video_config)
+    time.sleep(60)
 
-        cap.start()
-        cap.start_recording(encoder, output)
-
-        time.sleep(60)
-
-        cap.stop_recording()
-        
-except ImportError:
-    has_pi_camera = False
+    cap.stop_recording()
+    print("recording finished")
